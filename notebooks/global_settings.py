18c1726a9dcd36c0a1d1605648b8bc665aacd56b
Davide Calvaresi <imdavide@gmail.com>
2024-01-19 11:52:17 +0100

    introduced SimEx mode in settings and TODOs

--- notebooks/global_settings.py
+++ notebooks/global_settings.py
@@ -1,5 +1,6 @@
 # Overall SimEx settings
-simexSettings={"do_plot":False,"extensive_search":False,"extensive_iteration":False}
+# possible modes exploration (this one) and exploitation (mod with prob threes)
+simexSettings={"do_plot":False,"extensive_search":False,"extensive_iteration":False, "SimEx_mode":"exploration"}
 
 # Modifier domain settings
 mdv={"domain_min_range":1, "domain_max_range":100, "modifier_incremental_unit":3, "modifier_data_point":10}
@@ -19,3 +20,10 @@ vls={"points_fitting_total":0, "points_unfitting_total":0, "ranges_unfit_total":
 # Logger settings
 # log_granularity = ( 0 only general stats, 1 minimal log, 2 medium, 3 detailed)
 lgs={"log_granularity":3}
+
+
+
+# DDL: (for everything) END OF MARCH
+# TODO: 02/24 refactor unifying var naming
+# TODO: 02/24 log summary (timestamp | good interval | fitting function | fitting points -- and (at the end) unfitting ranges and points if any)
+# TODO: 03/24 a some point, introduce exploitation mode (just from modifier to sim and no validator)
