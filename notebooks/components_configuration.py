2fc1e5b191fb214c138e76ff395b784be84a1aea
Yvan Pannatier <yvan.pannatier@etu.unige.ch>
2024-04-10 16:11:17 +0200

    refactor: improve redability

--- notebooks/components_configuration.py
+++ notebooks/components_configuration.py
@@ -2,11 +2,9 @@ from modifier import Modifier
 from simulator import Simulator
 from validator import Validator
 
-# Modifier Function Settings
-selected_modifier = {'selectedModifier': Modifier.modifierA}
 
-# Simulator Function Settings
-selected_simulator = {'selectedSimulator': Simulator.sim_func_B}
-
-# Validator Function Settings
-selected_validator = {'selectedValidator': Validator.local_exploration_validator_A}
+components = {
+    'modifier': Modifier.modifierA,
+    'simulator': Simulator.sim_func_B,
+    'validator': Validator.local_exploration_validator_A
+}
