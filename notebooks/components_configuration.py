88d7bb7e96d2d40e2f7da19102e461ce70e043cc
Amy Liffey <amy.liffey@hes-so.ch>
2024-05-20 21:10:51 +0200

    feat: separate vsl and novsl sumo fce

--- notebooks/components_configuration.py
+++ notebooks/components_configuration.py
@@ -6,6 +6,7 @@ from validator import Validator
 components = {
     'modifier': Modifier.modifierA,
     'simulator': Simulator.sim_func_B,
-    'sumo_simulator': Simulator.sumo_simulator,
+    'sumo_simulator_vsl': Simulator.sumo_simulator_vsl,
+    'sumo_simulator_novsl': Simulator.sumo_simulator_novsl,
     'validator': Validator.local_exploration_validator_A
 }
