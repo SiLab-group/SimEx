78065f2dfff908b576d25aab6d5c2dbfd1924157
Amy Liffey <amy.liffey@hes-so.ch>
2024-06-24 16:33:16 +0200

    chore: specify global_settings

--- notebooks/global_settings.py
+++ notebooks/global_settings.py
@@ -1,8 +1,8 @@
 # Overall SimEx settings
 # possible modes exploration (this one) and exploitation (mod with prob threes)
-simexSettings = {"do_plot": False,  # No special meaning at the moment. Should be refactored.
+simexSettings = {"do_plot": False,  # No special meaning at the moment. TODO: Should be refactored.
                  "extensive_search": False,  #  Complete exploration setting modifier_data_point to 1 and enabling extensive iteration
-                 "extensive_iteration": False,  #  Gets enabled when extensive search is True
+                 "extensive_iteration": False,  #  Gets enabled when extensive search is True. TODO: should be refactored
                  "SimEx_mode": "exploration"}  # Only exploration implemented
 
 # Modifier Domain Settings
@@ -13,12 +13,15 @@ mds = {"domain_min_interval": 2500,
        }
 
 # Validator Function Settings
+# When fitting function we calculate MSE for each fitted function:
+# MSE ( (y_values, current_y_pred) + penality_weight * np.sum(current_coeff[:-1] ** 2) ) and compare it to the previous
+# We consider improvement acceptable if: (previous_mse - current_mse) >= improvement_threshold
 vfs = {'threshold_y_fitting': 15,  # Threshold on the y axis
-       'threshold_x_interval': 0.80,  # Percentage on x how good or bad point
-       'degree': 2,  # Minimum degree for exploration
-       'max_deg': 9,  # Max degree for exploration
-       'early_stop': True,  # Validator: if True and not sufficient improvement by increasing dimension, we stop
-       'improvement_threshold': 0.1,  # Sufficient improvement threshold
+       'threshold_x_interval': 0.80,  # For unfit point expand by threshold_x_interval to each side to close unfit interval
+       'degree': 2,  # Minimum degree for exploration. We start with polyfit in x^degree
+       'max_deg': 9,  # Max degree for exploration to which degree we try to fit function x^max_degree
+       'early_stop': True,  # if early_stop = True and improvement is not acceptable by increasing dimension, we stop
+       'improvement_threshold': 0.1,  # Sufficient improvement threshold (previous_mse - current_mse) >= improvement_threshold
        'penality_weight': 1}  # Penalty for MSE to avoid overfitting with high dimension polynomial
 
 ## Data and settings for log purposes ##
