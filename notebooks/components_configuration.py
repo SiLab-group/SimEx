47a6721634ebce93322379b5892a4a2fe7d40c19
Amy Liffey <amy.liffey@hes-so.ch>
2024-06-25 07:09:15 +0200

    chore: clean up old

--- notebooks/components_configuration.py
+++ notebooks/components_configuration.py
@@ -9,7 +9,5 @@ components = {
     'simulator': Simulator.sim_func_B,
     'sumo_simulator_vsl': Simulator.sumo_simulator_vsl,
     'sumo_simulator_novsl': Simulator.sumo_simulator_novsl,
-    'sumo_simulator_vsl_old': Simulator.sumo_simulator_vsl_old,
-    'sumo_simulator_novsl_old': Simulator.sumo_simulator_novsl_old,
     'validator': Validator.local_exploration_validator_A
 }
--- notebooks/simulator_controller.py
+++ notebooks/simulator_controller.py
@@ -12,27 +12,3 @@ class SimulatorController:
         simulated_y = [selected_simulator(x) for x in flat_mod_x]
         # print('  * Sim_y:   ', simulated_y)
         return flat_mod_x, simulated_y
-
-    # Amy test
-    def simulator(mod_x, selected_simulator):
-        simulated_y = []
-        print("Simulator...")
-        if mod_x is False:
-            return False  # Possible iterations have ended
-        ids = [mod_x[x][0] for x in range(0, len(mod_x))]
-        arrays = [mod_x[x][1] for x in range(0, len(mod_x))]
-        # print(f" IN SIMULATE arrays: {arrays}")
-        simulated_y = [selected_simulator(array) for array in arrays]
-        return ids, simulated_y
-
-    def sumo_simulate(mod_x, selected_simulator):
-        simulated_y = []
-        print(f"Simulator... {mod_x}")
-        if mod_x is False:
-            return False  # Possible iterations have ended
-        flat_mod_x = [item for sublist in mod_x for item in sublist]
-        arrays = [[i] * 90 for i in flat_mod_x]
-        print(f" IN SIMULATE arrays: {arrays}")
-        print(f" IN SIMULATE arrays: {mod_x}")
-        simulated_y = [selected_simulator(array) for array in arrays]
-        return flat_mod_x,simulated_y
